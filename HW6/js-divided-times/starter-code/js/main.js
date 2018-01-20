83c04eccb075741ea53dded00afce11bdcaad66a
Adrianne <adrianne.ngam@gmail.com>
2018-01-20 07:42:49 -0800

    Second Commit

--- HW6/js-divided-times/starter-code/js/main.js
+++ HW6/js-divided-times/starter-code/js/main.js
@@ -20,40 +20,84 @@
     });
 
 	$(".politics").on('click', function(e){
-		$(".drop").hide();
-		$('#politics-drop').show();
-		$('#slide-down').slideDown();
-		console.log("test")
-	});
+        event.preventDefault();
+        $('li').attr('class', '')
+        if ($('.politics').hasClass('open')) {
+            $("#slide-down").slideUp();
+            $('.politics').removeClass('open');
+        }
+        else {
+            $('.drop').hide();
+            $('#politics-drop').show();
+            $('#slide-down').slideDown();
+            $(".politics").addClass('open');
+            $(this).addClass('active');
+        }
+    });
 
 	$(".business").on('click', function(e){
-		$(".drop").hide();
-		$('#business-drop').show();
-		$('#slide-down').slideDown();
-		console.log("test")
-	});
+        event.preventDefault();
+        $('li').attr('class', '')
+        if ($('.business').hasClass('open')) {
+            $("#slide-down").slideUp();
+            $('.business').removeClass('open');
+        }
+        else {
+            $('.drop').hide();
+            $('#business-drop').show();
+            $('#slide-down').slideDown();
+            $(".business").addClass('open');
+            $(this).addClass('active');
+        }
+    });
 
 	$(".technology").on('click', function(e){
-		$(".drop").hide();
-		$('#technology-drop').show();
-		$('#slide-down').slideDown();
-		console.log("test")
-	});
+        event.preventDefault();
+        $('li').attr('class', '')
+        if ($('.technology').hasClass('open')) {
+            $("#slide-down").slideUp();
+            $('.technology').removeClass('open');
+        }
+        else {
+            $('.drop').hide();
+            $('#technology-drop').show();
+            $('#slide-down').slideDown();
+            $(".technology").addClass('open');
+            $(this).addClass('active');
+        }
+    });
 
 	$(".culture").on('click', function(e){
-		$(".drop").hide();
-		$('#culture-drop').show();
-		$('#slide-down').slideDown();
-		console.log("test")
-	});
+        event.preventDefault();
+        $('li').attr('class', '')
+        if ($('.culture').hasClass('open')) {
+            $("#slide-down").slideUp();
+            $('.culture').removeClass('open');
+        }
+        else {
+            $('.drop').hide();
+            $('#culture-drop').show();
+            $('#slide-down').slideDown();
+            $(".culture").addClass('open');
+            $(this).addClass('active');
+        }
+    });
 
 	$(".blogs").on('click', function(e){
-		$(".drop").hide();
-		$('#blogs-drop').show();
-		$('#slide-down').slideDown();
-		console.log("test")
-	});
-
+        event.preventDefault();
+        $('li').attr('class', '')
+        if ($('.blogs').hasClass('open')) {
+            $("#slide-down").slideUp();
+            $('.blogs').removeClass('open');
+        }
+        else {
+            $('.drop').hide();
+            $('#blogs-drop').show();
+            $('#slide-down').slideDown();
+            $(".blogs").addClass('open');
+            $(this).addClass('active');
+        }
+    });
 
 	//If div Is dropped down
 		//#slide-down stays as is
